77f6a7da8959018f527a324906d627c5a73c4a5f
The Dogemon <epicsqueebo@gmail.com>
2022-07-03 23:44:52 -0400

    Modified start_game.bat for multiplayer purposes as needed.

--- win32/start_game.bat
+++ win32/start_game.bat
@@ -8,6 +8,10 @@ rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
 set LOGIN_TOKEN=dev
+set /P LOGIN_TOKEN="Username: "
+
+set GAME_SERVER=127.0.0.1
+set /P GAME_SERVER="Gameserver: "
 
 %PPYTHON_PATH% -m toontown.launcher.QuickStartLauncher
 pause
