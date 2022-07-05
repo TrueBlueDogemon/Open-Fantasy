fa9416523ef631c5c5c75b06d881debb9a6e589d
The Dogemon <epicsqueebo@gmail.com>
2022-07-04 20:51:24 -0400

    Reverted most changes to start_game.bat, but still, MP is re-enabled.

--- win32/start_game.bat
+++ win32/start_game.bat
@@ -4,8 +4,7 @@ title Open-Fantasy Game Launcher
 
 echo Choose your connection method!
 echo.
-echo #1 - Offline Mode
-echo #2 - Custom Server
+echo #1 - Offline (specify a server to play Custom Mode)
 echo.
 
 :selection
@@ -15,33 +14,40 @@ set /P INPUT=Selection:
 
 if %INPUT%==1 (
     set TT_GAMESERVER=127.0.0.1
-) elif INPUT=2 (
     set /P TT_GAMESERVER="Gameserver: "
 ) else (
-    echo Oops! Try selecting again.
     goto selection
 )
 
 echo.
 
-    set LOGIN_TOKEN=dev
+if %INPUT%==1 (
     set /P LOGIN_TOKEN="Username: "
+) else (
+    set /P LOGIN_TOKEN=Username:
+)
 
 echo.
 
 echo ===============================
 echo Starting Toontown Fantasy
 
-echo Username: %LOGIN_TOKEN%
-echo Gameserver: %TT_GAMESERVER%
+if %INPUT%==1 (
+    echo Username: %ttUsername%
+) else (
+    echo Username: %TT_PLAYCOOKIE%
+)
 
+echo Gameserver: %TT_GAMESERVER%
 echo ===============================
 
 cd ../
 
 :main
+if %INPUT%==1 (
+    "C:\Panda3D-1.11.0-x64\python\ppython.exe" -m toontown.launcher.QuickStartLauncher
+)
 
-"C:\Panda3D-1.11.0-x64\python\ppython.exe" -m toontown.launcher.QuickStartLauncher
 pause
 
 goto main
